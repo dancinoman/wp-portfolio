b00c06a1f6696a1ba6685155a626a58c0ef29775
dancinoman <dancinoman@gmail.com>
2018-02-19 23:09:57 -0500

    new header text

--- wordpress/wp-content/themes/portfolio/index.php
+++ wordpress/wp-content/themes/portfolio/index.php
@@ -1,5 +1,12 @@
 <?php get_header(); ?>
 
-<div class="header-text"><div>
+<div class="header-text">
+  <p>
+    <?php
+      $post = get_post(11);
+      echo $post->post_content;
+    ?>
+  </p>
+<div>
 
 <?php get_footer(); ?>
